475c072001e7bba561aeceb8984abb3212fa2224
Darren Banfi <boingball@gmail.com>
2026-06-23 22:30:19 +0100

    Remove printf from AAC SBR code

--- src/libhelix-aac/sbr.c
+++ src/libhelix-aac/sbr.c
@@ -107,7 +107,6 @@ int InitSBR(AACDecInfo *aacDecInfo) {
     /* allocate SBR state structure */
     psi = (PSInfoSBR *)malloc(sizeof(PSInfoSBR));
     if (!psi) {
-        printf("OOM in SBR, can't allocate %d bytes\n", sizeof(PSInfoSBR));
         return ERR_AAC_SBR_INIT;
     }
     InitSBRState(psi);
@@ -127,7 +126,6 @@ int InitSBRPre(AACDecInfo *aacDecInfo, void **ptr, int *sz) {
     psi = (PSInfoSBR *)*ptr;
     *sz -= sizeof(PSInfoSBR);
     if (*sz < 0) {
-        printf("OOM in SBR, can't allocate %d bytes\n", sizeof(PSInfoSBR));
         return ERR_AAC_SBR_INIT;
     }
     InitSBRState(psi);
